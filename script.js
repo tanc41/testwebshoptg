9ea7ff6b44d9a6a44aa53ae68f1ef90345b4968e
tanc41 <33790473+tanc41@users.noreply.github.com>
2023-09-09 04:54:38 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function orderService(service) {
-    // Store the selected service in local storage or a cookie
-    localStorage.setItem('selectedService', service);
-
-    // Redirect to the checkout page
-    window.location.href = 'checkout.html';
-}
